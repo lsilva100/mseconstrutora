be6b2c25e1df0ff8a456149d6e4a5e2369d2934f
Lucas <135278098+lsilva100@users.noreply.github.com>
2024-04-23 16:28:21 -0300

    Update script.js
    Retirada do G.A

--- JS/script.js
+++ JS/script.js
@@ -93,14 +93,6 @@ function toggleMenu() {
 /***************************************************************************/
 
 
-async src="https://www.googletagmanager.com/gtag/js?id=G-1R8TJJ3V6D"
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-1R8TJJ3V6D');
-
-
 
 
 
